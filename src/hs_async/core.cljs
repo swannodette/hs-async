bc120641033199cbec42d433dc04a5837d8a6789
David Nolen <david.nolen@gmail.com>
2013-07-20 19:56:26 -0400

    add rob pike example

--- src/hs_async/core.cljs
+++ src/hs_async/core.cljs
@@ -151,7 +151,7 @@
 (defn process [name control]
   (let [out  (chan)
         ints (my-ints)
-        tick (interval 100)]
+        tick (interval 1000)]
     (go
       (<! control)
       (.log js/console "start" name)
@@ -167,10 +167,9 @@
               (recur (+ acc (<! ints))))))))
     out))
 
-(defn now []
-  (js/Date.))
+(defn now [] (js/Date.))
 
-(let [c0    (chan)
+#_(let [c0    (chan)
       c1    (chan)
       out   (fan-in [(process "p0" c0) (process "p1" c1)])
       keys  (->> (events js/window "keyup")
@@ -188,3 +187,39 @@
           ([v] (case state
                  0 (do (>! c0 (now)) (>! c1 (now)) 1)
                  1 (do (>! c1 (now)) (>! c0 (now)) 0))))))))
+
+;; =============================================================================
+;; Escaping Callback hell
+
+(defn fake-search [kind]
+  (fn [c query]
+    (go
+     (<! (timeout (rand-int 100)))
+     (>! c [kind query]))))
+
+(def web1 (fake-search :web1))
+(def web2 (fake-search :web2))
+(def image1 (fake-search :image1))
+(def image2 (fake-search :image2))
+(def video1 (fake-search :video1))
+(def video2 (fake-search :video2))
+
+(defn fastest [query & replicas]
+  (let [c (chan)]
+    (doseq [replica replicas]
+      (replica c query))
+    c))
+
+(defn google [query]
+  (let [c (chan)
+        t (timeout 80)]
+    (go (>! c (<! (fastest query web1 web2))))
+    (go (>! c (<! (fastest query image1 image2))))
+    (go (>! c (<! (fastest query video1 video2))))
+    (go (loop [i 0 ret []]
+          (if (= i 3)
+            ret
+            (recur (inc i) (conj ret (alt! [c t] ([v] v)))))))))
+
+#_(go (.log js/console (pr-str (<! (google "clojure")))))
+
